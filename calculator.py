a404471d7fb983696611b3b5f4cd8205be74dddf
njiddasalifu <njiddasalifu@gmail.com>
2023-06-20 12:44:40 +0100

    small age calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+
+# # name = "salif"
+# # print("Hey! "+name)
+
+# #asking the user for input
+# name  =  finput("Please enter your name:")
+# print("Hey! "+name+" how are you doing today?")
+
+year = input("Enter your birth year:")
+print("You were born in "+year)
+age = 2023 - int(year)
+urage=str(age)
+
+print("You are " +urage+ "years old")
