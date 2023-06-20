02e27a55d936f975b5d0913e0d1a9f30b9429a45
njiddasalifu <njiddasalifu@gmail.com>
2023-06-20 15:03:29 +0100

    playwithpython

---
+++ playwithpython/calculator.py
@@ -0,0 +1,50 @@
+
+
+# num1 = input("Enter first number:")
+# num2 = input("Enter second number:")
+
+# num1 = int(num1)
+# num2 = int(num2)
+
+# print("1. Add\n 2. Subtract\n 3. Multiply")
+# choice = input("What do you want to do? ")
+# if choice == "1":
+#     sum = num1 + num2
+#     print(sum)
+# elif choice == "2":
+#     sub = num1 - num2
+#     print(sub)
+
+# elif choice == "3":
+#     mul = num1 * num2
+#     print(mul)
+# else :
+#     print("No entry")
+
+
+# # switch (choice)
+# # case 1:
+# # num1 + num2
+# # break
+# # case 2:
+# # num1 - num2
+# # break
+# # case 3:
+# # num1 * num2
+# # break
+# # default:
+
+
+# # print("You are " +age+ " years old")
+
+# # num1 = 6
+# # num2 = 2
+# # sum = num1 + num2
+# # sub = num1 - num2
+# # divie = num1 / num2
+# # mul = num1 * num2
+# # print(sum)
+# # print(str(sum))
+# # print(mul)
+
+
